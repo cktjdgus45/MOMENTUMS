645c7e3392207539fa9c28dd9c8963143e549f1c
pooh <cktjdgus45@naver.com>
2022-01-20 21:32:23 +0900

    make login state,logout state UI

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const nameInput = document.querySelector('.center-input');
 const centerForm = document.querySelector('.center-form');
+const nameFormContainer = document.querySelector('.center-nameForm-container');
+const todoFormContainer = document.querySelector('.center-todoForm-container');
+const userName = document.querySelector('.userName');
 
 const localStorage = window.localStorage;
 
+if (localStorage.getItem('name')) {
+    todoFormContainer.classList.replace('hide', 'show');
+    nameFormContainer.classList.replace('show', 'hide');
+    userName.innerText = `${localStorage.getItem('name')}`;
+}
+
+
 const handleSubmit = (event) => {
     event.preventDefault();
-    const username = nameInput.value;
-    localStorage.setItem('name', username);
+    const user = nameInput.value;
+    localStorage.setItem('name', user);
     nameInput.value = "";
-    console.log(localStorage.getItem('name'))
+    // console.log(localStorage.getItem('name'))
+    nameFormContainer.classList.replace('show', 'hide');
+    todoFormContainer.classList.replace('hide', 'show');
+    userName.innerText = `${localStorage.getItem('name')}`;
 }
 
 centerForm.addEventListener('submit', handleSubmit);
