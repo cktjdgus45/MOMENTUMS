4d0b3d1a2c60a1de9df34c6ada89d83f7e0d2f5d
pooh <cktjdgus45@naver.com>
2022-01-21 22:02:35 +0900

    read and write todos to localstorage

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-const nameInput = document.querySelector('.center-input');
-const centerForm = document.querySelector('.center-form');
+import User from './service/User.js';
+
+const nameInput = document.querySelector('.center-name-input');
+const todoInput = document.querySelector('.center-todo-input');
+const nameForm = document.querySelector('.center-name-form');
+const todoForm = document.querySelector('.center-todo-form');
 const nameFormContainer = document.querySelector('.center-nameForm-container');
 const todoFormContainer = document.querySelector('.center-todoForm-container');
-const userName = document.querySelector('.userName');
+const userNameElement = document.querySelector('.userName');
 
-const localStorage = window.localStorage;
+const user = new User();
 
-if (localStorage.getItem('name')) {
+const handleFormDisplay = () => {
     todoFormContainer.classList.replace('hide', 'show');
     nameFormContainer.classList.replace('show', 'hide');
-    userName.innerText = `${localStorage.getItem('name')}`;
 }
 
+if (user.isLogin()) {
+    handleFormDisplay();
+    userNameElement.innerText = user.getName();
+}
 
-const handleSubmit = (event) => {
+const handleNameSubmit = (event) => {
     event.preventDefault();
-    const user = nameInput.value;
-    localStorage.setItem('name', user);
+    const userName = nameInput.value;
+    user.setName(userName);
     nameInput.value = "";
-    // console.log(localStorage.getItem('name'))
-    nameFormContainer.classList.replace('show', 'hide');
-    todoFormContainer.classList.replace('hide', 'show');
-    userName.innerText = `${localStorage.getItem('name')}`;
+    handleFormDisplay();
+    userNameElement.innerText = user.getName();
 }
 
-centerForm.addEventListener('submit', handleSubmit);
+const handleTodoSubmit = (event) => {
+    event.preventDefault();
+    const todo = todoInput.value;
+    todoInput.value = "";
+    user.setTodo(todo);
+    user.getTodo();
+}
+
+nameForm.addEventListener('submit', handleNameSubmit);
+todoForm.addEventListener('submit', handleTodoSubmit);
---
+++ service/User.js
@@ -0,0 +1,30 @@
+export default class User {
+    todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+    userName;
+    setName(userName) {
+        localStorage.setItem('name', userName);
+    }
+    getName() {
+        this.userName = localStorage.getItem('name');
+        return this.userName;
+    }
+    isLogin() {
+        if (this.getName()) {
+            return this.getName();
+        } else {
+            return false;
+        }
+    }
+    setTodo(todoValue) {
+        const todo = {
+            id: this.todos.length,
+            todo: todoValue
+        };
+        this.todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+    getTodo() {
+        const todos = localStorage.getItem('todos');
+        console.log(todos);
+    }
+}
